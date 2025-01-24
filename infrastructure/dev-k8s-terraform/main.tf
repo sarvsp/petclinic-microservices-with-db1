9882c6615aff18a7f3173a599ceb849ec62c9270
sarvsp <sarvanipamarti0225@gmail.com>
2025-01-24 10:56:47 +0000

    added dev-k8s-terraform  for kubernetes infrastructure

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -89,7 +89,7 @@ resource "aws_instance" "kube-master" {
   instance_type = "t3a.medium"
   iam_instance_profile = aws_iam_instance_profile.petclinic-master-server-profile.name
   vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
-  key_name = "clarus"
+  key_name = "petclinic-ansible-test-dev.key"
   subnet_id = "subnet-0f09db7ff321dec20"  # select own subnet_id of us-east-1a
   availability_zone = "us-east-1a"
   tags = {
@@ -105,8 +105,8 @@ resource "aws_instance" "worker-1" {
   ami = "ami-005fc0f236362e99f"
   instance_type = "t3a.medium"
   vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
-  key_name = "clarus"
-  subnet_id = "subnet-06f28877248571c31"  # select own subnet_id of us-east-1a
+  key_name = "petclinic-ansible-test-dev.key"
+  subnet_id = "subnet-0f09db7ff321dec20"  # select own subnet_id of us-east-1a
   availability_zone = "us-east-1a"
   tags = {
     Name = "worker-1"
@@ -121,8 +121,8 @@ resource "aws_instance" "worker-2" {
   ami = "ami-005fc0f236362e99f"
   instance_type = "t3a.medium"
   vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
-  key_name = "clarus"
-  subnet_id = "subnet-053a0d3411a8dec71"  # select own subnet_id of us-east-1a
+  key_name = "petclinic-ansible-test-dev.key"
+  subnet_id = "subnet-0f09db7ff321dec20"  # select own subnet_id of us-east-1a
   availability_zone = "us-east-1a"
   tags = {
     Name = "worker-2"
